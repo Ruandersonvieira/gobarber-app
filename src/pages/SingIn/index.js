325abb8f9b61ea73e9ef2b1af7f07e023a7b25c9
Ruanderson Vieira <ruanderson.vieira@accalia.io>
2020-03-28 22:09:34 -0300

    :sparkles: singin screan

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import Logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+
+import {
+  Container,
+  Image,
+  Form,
+  FormInput,
+  SubmitButton,
+  SingLink,
+  SingLinkText,
+} from './styles';
 
 export default function SingIn() {
   return (
     <Background>
-      <Text> Sing In </Text>
+      <Container>
+        <Image source={Logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SingLink onPress={() => {}}>
+          <SingLinkText>Criar conta gratuita</SingLinkText>
+        </SingLink>
+      </Container>
     </Background>
   );
 }
