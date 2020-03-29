dc97039097c4a6b259a1469394a19ddb48ac9be9
Ruanderson Vieira <ruanderson.vieira@accalia.io>
2020-03-29 10:27:33 -0300

    :heavy_plus_sign: redux saga

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 
-import './config/ReactotronConfig';
+import '~/config/ReactotronConfig';
 
+import { store, persistor } from '~/store';
 import Routes from '~/routes';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <Routes />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
