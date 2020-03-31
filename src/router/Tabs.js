967856e58ddc257f64e5402208c62b888bd53192
Ruanderson Vieira <ruanderson.vieira@accalia.io>
2020-03-30 21:49:55 -0300

    :art: tab icon

--- src/router/Tabs.js
+++ src/router/Tabs.js
@@ -2,13 +2,37 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Dashboard from '~/pages/Dashboard';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function Application() {
   return (
-    <Tab.Navigator independent={1}>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+    <Tab.Navigator
+      independent={1}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Agendamentos') {
+            iconName = 'event';
+          } else if (route.name === 'Agendamentos') {
+            iconName = 'person';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        activeTintColor: '#FFF',
+        inactiveTintColor: 'rgba(255,255,255, 0.6)',
+        style: {
+          backgroundColor: '#8b41a8',
+        },
+      }}
+    >
+      <Tab.Screen name="Agendamentos" component={Dashboard} />
     </Tab.Navigator>
   );
 }
